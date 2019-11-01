1d1420221375afb86dc18d349bcbca0d550f109a
yingxiu <200810048a@163.com>
2019-11-01 16:57:47 +0800

    同步概念介绍，异步编程就时为了让程序在一个时间内做更多的事情

---
+++ JS_通用异步编程概念/simple_sync_blocking.js
@@ -0,0 +1,14 @@
+function expensiveOperation() {
+    for(let i = 0; i < 1000000; i++) {
+      ctx.fillStyle = 'rgba(0,0,255, 0.2)';
+      ctx.beginPath();
+      ctx.arc(random(0, canvas.width), random(0, canvas.height), 10, degToRad(0), degToRad(360), false);
+      ctx.fill()
+    }
+  }
+  
+  fillBtn.addEventListener('click', expensiveOperation);
+  
+  alertBtn.addEventListener('click', () =>
+    alert('You clicked me!')
+  );

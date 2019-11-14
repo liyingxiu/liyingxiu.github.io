6018803b7cb677bee723c91a8aa5288e327f0d09
yingxiu <200810048a@163.com>
2019-11-14 21:39:48 +0800

    更新已用时间，修复播放和暂停，比较难理解，需要好好看

--- 视频与音频API/custom-player.js
+++ 视频与音频API/custom-player.js
@@ -32,15 +32,19 @@ function playPauseMedia() {
     }
 }
 
-//视频暂停的方法
-stop.addEventListener('click', stopMedia);
-media.addEventListener('ended', stopMedia);
-
 function stopMedia() {
     media.pause();
     media.currentTime = 0;
     play.setAttribute('data-icon', 'P');
+    rwd.classList.remove('active');
+    fwd.classList.remove('active');
+    clearInterval(intervalRwd);
+    clearInterval(intervalFwd);
 }
+
+//视频暂停的方法
+stop.addEventListener('click', stopMedia);
+media.addEventListener('ended', stopMedia);
 
 
 //探索快进和快退
@@ -78,4 +82,52 @@ function mediaForward() {
         media.pause();
         intervalFwd = setInterval(windForward, 200);
     }
+}
+
+function windBackward() {
+    if (media.currentTime <= 3) {
+        rwd.classList.remove('active');
+        clearInterval(intervalRwd);
+        stopMedia();
+    } else {
+        media.currentTime -= 3;
+    }
+}
+
+function windForward() {
+    if (media.currentTime >= media.duration - 3) {
+        fwd.classList.remove('active');
+        clearInterval(intervalFwd);
+        stopMedia();
+    } else {
+        media.currentTime += 3;
+    }
+}
+
+
+//更新已用时间
+media.addEventListener('timeupdate', setTime);
+function setTime() {
+    var minutes = Math.floor(media.currentTime / 60);
+    var seconds = Math.floor(media.currentTime - minutes * 60);
+    var minuteValue;
+    var secondValue;
+
+    if (minutes < 10) {
+        minuteValue = '0' + minutes;
+    } else {
+        minuteValue = minutes;
+    }
+
+    if (seconds < 10) {
+        secondValue = '0' + seconds;
+    } else {
+        secondValue = seconds;
+    }
+
+    var mediaTime = minuteValue + ':' + secondValue;
+    timer.textContent = mediaTime;
+
+    var barLength = timerWrapper.clientWidth * (media.currentTime / media.duration);
+    timerBar.style.width = barLength + 'px';
 }
